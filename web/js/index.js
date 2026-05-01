80a946d7311f8424a97cdb67ebad0a5686703796
Jose Manuel GV <jmgv.ciencia@gmail.com>
2026-05-01 20:27:50 +0200

    Incrustra en la galeria fotos a partir de un array de fotos

--- web/js/index.js
+++ web/js/index.js
@@ -1,17 +1,42 @@
 "use strict";
 import { parseHTML } from "./utils/parseHTML.js";
-import { photoRenderer } from "./renderers/photos.js";
+import { galleryRenderer } from "./renderers/gallery.js";
 
 function main() {
-    let container = document.getElementById("gallery");
-    let photo = {
-        title: "Samoyed",
-        description: "A very good boy.",
-        userId: 1,
-        url: "https://i.ibb.co/tY1Jcnc/wlZCfCv.jpg",
-    };
-    let card = photoRenderer.asCard(photo);
-    container.appendChild(card);
+  let container = document.getElementById("gallery");
+  let photos = [
+    {
+      title: "Samoyed",
+      description: "A very good boy.",
+      userId: 1,
+      url: "https://i.ibb.co/tY1Jcnc/wlZCfCv.jpg",
+      date: "15/08/2020",
+    },
+    {
+      title: "ETSII",
+      description: "E.T.S. Ing. Informatica, Universidad de Sevilla",
+      userId: 2,
+      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/ETSI_Inform%C3%A1tica_Sevilla_y_DrupalCamp_Spain_2011.jpg/1920px-ETSI_Inform%C3%A1tica_Sevilla_y_DrupalCamp_Spain_2011.jpg",
+      date: "01/01/2021",
+    },
+    {
+      title: "Seville",
+      description: "The beautiful city of Seville",
+      userId: 3,
+      url: "https://urbansevilla.es/wp-content/uploads/2019/03/urban-sevilla-foto-ciudad.jpg",
+      date: "03/02/2019",
+    },
+    {
+      title: "Abstract art",
+      description: "Clipart",
+      userId: 4,
+      url: "https://i.ibb.co/tY1Jcnc/wlZCfCv.jpg",
+      date: "14/08/2019",
+    },
+  ];
+
+  let gallery = galleryRenderer.asCardGallery(photos);
+  container.appendChild(gallery);
 }
 
 document.addEventListener("DOMContentLoaded", main);
