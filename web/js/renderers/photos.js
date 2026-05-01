c4f3bc8d04a2f20aecbca85afa18f26426e15181
Jose Manuel GV <jmgv.ciencia@gmail.com>
2026-05-01 20:36:50 +0200

    crea un nuevo metodo en el renderizado de fotos para obtener los detalles de una foto

--- web/js/renderers/photos.js
+++ web/js/renderers/photos.js
@@ -2,10 +2,9 @@
 
 import { parseHTML } from "../utils/parseHTML.js";
 
-
 const photoRenderer = {
-    asCard: function(photo) {
-        let html = `<div class="col-md-4">
+  asCard: function (photo) {
+    let html = `<div class="col-md-4">
       <div class="card bg-dark text-light">
         <img src="${photo.url}" class="card-img-top">
 
@@ -19,7 +18,22 @@ const photoRenderer = {
 
     let card = parseHTML(html);
     return card;
-    }
+  },
+  
+  asDetails: function (photo) {
+    let html = `<div class="photo-details">
+        <h3>${photo.title}</h3>
+        <h6>${photo.description}</h6>
+        <p>Uploaded by <a href="user_profile.html" class="user-link">User ${photo.userId}</a> on ${photo.date}</p>
+
+        <hr>
+
+        <img src="${photo.url}" class="img-fluid">
+      </div>`;
+
+    let photoDetails = parseHTML(html);
+    return photoDetails;
+  },
 };
 
 export { photoRenderer };
