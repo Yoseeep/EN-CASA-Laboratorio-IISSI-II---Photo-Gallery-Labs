f7f2ec27f8653ff736fcbd1055ad7b258f90181b
Jose Manuel GV <jmgv.ciencia@gmail.com>
2026-05-12 18:26:09 +0200

    Implementa el log out en el header

--- web/js/header.js
+++ web/js/header.js
@@ -4,6 +4,7 @@ import { sessionManager } from "./utils/session.js";
 
 function main() {
     showUser();
+    addLogoutHandler();
 }
 
 function showUser() {
@@ -18,6 +19,15 @@ function showUser() {
     }
 
     title.textContent = text;
+}
+
+function addLogoutHandler() {
+    let logoutButton = document.getElementById("navbar-logout");
+
+    logoutButton.onclick = function () {
+        sessionManager.logout();
+        window.location.href = "index.html";
+    };
 }
 
 document.addEventListener("DOMContentLoaded", main);
