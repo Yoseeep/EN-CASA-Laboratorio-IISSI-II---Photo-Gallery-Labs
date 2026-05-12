eabe2468cd5c3b14be8519f1dbe96a19e8d0197a
Jose Manuel GV <jmgv.ciencia@gmail.com>
2026-05-12 17:22:33 +0200

    Implementa el validador en la pagina de registro

--- web/js/register.js
+++ web/js/register.js
@@ -1,6 +1,7 @@
 "use strict";
 
 import { messageRenderer } from "./renderers/messages.js";
+import { userValidator } from "./validators/users.js";
 
 function main() {
   let registerForm = document.querySelector("#register-form");
@@ -11,7 +12,7 @@ function handleSubmitRegister(event) {
   let form = event.target;
   let formData = new FormData(form);
 
-  
+  let errors = userValidator.validateRegister(formData);
 
   if (errors.length > 0) {
         event.preventDefault();
